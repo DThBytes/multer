0b5983bb6fd9e98432fc9c56b88bfe265cd6609e
Diego Theriano <dtheriano@gmail.com>
2023-05-25 16:56:43 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// require the installed packages 
+const express = require('express')
+const multer = require('multer');
+//CREATE EXPRESS APP 
+const app = express();
+//ROUTES WILL GO HERE 
+app.get('/', function(req, res) {
+    res.json({ message: 'WELCOME' });   
+});
+app.listen(3000, () => 
+    console.log('Server started on port 3000')
+);
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname+'/uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+  var upload = multer({ storage: storage })
+
+  app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      res.send(file)
+   
+  })
