792bc540adbe088177077a5d11125fddf71b6457
DThBytes <dtheriano@gmail.com>
2023-05-29 17:45:34 -0300

    list of files

--- server.js
+++ server.js
@@ -11,24 +11,45 @@ app.listen(80, () =>
     console.log('Server started on port 80')
 );
 
+// VARS
+const uploadFolder = '/uploads';
+
 // SET STORAGE
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, __dirname+'/uploads')
+      cb(null, __dirname + uploadFolder)
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalname + '-' + Date.now())
+      cb(null, file.originalname + '-' + new Date().toISOString().slice(0,10))
     }
-  })
-  var upload = multer({ storage: storage })
+});
 
-  app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+var upload = multer({ storage: storage });
+
+app.post('/uploadfile', upload.single('file'), (req, res, next) => {
     const file = req.file
     if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next(error)
+      const error = new Error('Please upload a file');
+      error.httpStatusCode = 400;
+      return next(error);
     }
-      res.send(file)
-   
-  })
+      res.send(file);   
+});
+
+// LIST FILES
+app.get('/listfiles', (req, res) => {
+    //res.json({ message: 'List of files' });  
+    const fs = require('fs');
+    const listFiles=[];
+    fs.readdir(__dirname + uploadFolder, (err, files) => {
+      files.forEach(file => {
+        const fileDetail = {
+          File: file,
+          Date: file.slice(-10),
+          Size: fs.statSync(__dirname + uploadFolder+ '/'+file).size
+        };
+        listFiles.push(fileDetail);
+      });
+      res.json(listFiles);
+    }); 
+});
